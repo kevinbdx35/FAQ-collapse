70610cdd05ec642d741dd5de2ddf31b61659e5fe
kevinbdx35 <k.bardoux@orange.fr>
2022-01-08 15:46:15 +0100

    init commit ✔️

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach(function (toggle) {
+  toggle.addEventListener("click", function () {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
